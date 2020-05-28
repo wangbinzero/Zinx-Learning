46f5d3b888b4b538c8c87f75e875e209a072e60a
wangbinzero <wangbbhtt@gmail.com>
2020-05-28 09:42:32 +0800

    封装request请求

--- znet/request.go
+++ znet/request.go
@@ -3,14 +3,16 @@ package znet
 import "Zinx-Learning/ziface"
 
 type Request struct {
-	conn ziface.IConnection
-	data []byte
+	conn ziface.IConnection //已经和客户端建立好的连接
+	data []byte             //客户端请求的数据
 }
 
+//获取连接信息
 func (r *Request) GetConnection() ziface.IConnection {
 	return r.conn
 }
 
+//获取请求数据
 func (r *Request) GetData() []byte {
 	return r.data
 }
