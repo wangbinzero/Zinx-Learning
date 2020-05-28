4cb4e239f377445aa2842fce82588e012a281867
wangbinzero <wangbbhtt@gmail.com>
2020-05-28 12:05:02 +0800

    测试 router逻辑

--- test_server.go
+++ test_server.go
@@ -1,9 +1,33 @@
 package main
 
-import "Zinx-Learning/znet"
+import (
+	"Zinx-Learning/ziface"
+	"Zinx-Learning/znet"
+	"fmt"
+)
 
 func main() {
 
 	s := znet.NewServer("zinx v0.1")
+	s.AddRouter(&PingRouter{})
 	s.Serve()
+}
+
+//自定义路由
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+func (pr *PingRouter) PreHandle(request ziface.IRequest) {
+	fmt.Println("custom preHandle router")
+	request.GetConnection().GetTCPConnection().Write([]byte("before ping"))
+}
+
+func (pr *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("custom handle router")
+	request.GetConnection().GetTCPConnection().Write([]byte("handle ping"))
+}
+
+func (pr *PingRouter) PostHandle(request ziface.IRequest) {
+	fmt.Println("custom postHandle router")
 }
